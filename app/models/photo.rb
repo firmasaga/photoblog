305ed36552f66c33094d01e334c7c5e768690a1b
Daniel Schlegel <firmasaga@gmx.net>
2009-11-15 17:23:57 +0100

    added photoblog

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
   
   has_many :comments
   
-  has_attached_file :source#, :styles => { :medium => "600x300", :small  => "300x100>" }
+  has_attached_file :source, :styles => { :medium => "600x300", :small  => "300x100>" }
 
   validates_presence_of :title
   validates_attachment_presence :source
